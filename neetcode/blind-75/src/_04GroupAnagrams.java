ef5be9c6060efc36cbcb4c786938ca98bb0b3cf2
iavinash <iavinashmishra@gmail.com>
2025-08-31 23:33:05 +0530

    Neetcode-75: Init Group Anagrams

---
+++ neetcode/blind-75/src/_04GroupAnagrams.java
@@ -0,0 +1,33 @@
+/**
+ * Author: Avinash Mishra
+ * User: avinash
+ * Date: 31/08/25
+ */
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Given an array of strings strs, group all anagrams together into sublists. You may return the output in any order.
+ * <p>
+ * An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+ * <br>
+ * Example 1:
+ * <p>
+ * Input: strs = ["act","pots","tops","cat","stop","hat"]
+ * <p>
+ * Output: [["hat"],["act", "cat"],["stop", "pots", "tops"]]
+ * <br>
+ * Input: strs = ["x"]
+ * <p>
+ * Output: [["x"]]
+ */
+public class _04GroupAnagrams {
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        Map<String, List<String>> map = new HashMap<>();
+        return null;
+    }
+}
