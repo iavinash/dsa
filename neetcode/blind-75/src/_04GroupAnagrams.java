92468e54e7e1559c7b3f5e5e30ba56d56f2bac5a
iavinash <iavinashmishra@gmail.com>
2025-09-01 20:52:31 +0530

    Neetcode-75: Group Anagrams

--- neetcode/blind-75/src/_04GroupAnagrams.java
+++ neetcode/blind-75/src/_04GroupAnagrams.java
@@ -4,6 +4,8 @@
  * Date: 31/08/25
  */
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -11,7 +13,8 @@ import java.util.Map;
 /**
  * Given an array of strings strs, group all anagrams together into sublists. You may return the output in any order.
  * <p>
- * An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+ * An anagram is a string that contains the exact same characters as another string,
+ * but the order of the characters can be different.
  * <br>
  * Example 1:
  * <p>
@@ -28,6 +31,29 @@ public class _04GroupAnagrams {
     public List<List<String>> groupAnagrams(String[] strs) {
 
         Map<String, List<String>> map = new HashMap<>();
-        return null;
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String sorted = new String(chars);
+            if (!map.containsKey(sorted)) {
+                List<String> list = new ArrayList<>();
+                list.add(str);
+                map.put(sorted, list);
+            } else {
+                map.get(sorted).add(str);
+            }
+        }
+        List<List<String>> result = new ArrayList<>();
+        for (Map.Entry<String, List<String>> entry : map.entrySet()) {
+            result.add(new ArrayList<>(entry.getValue()));
+        }
+
+        return result;
+    }
+    public static void main(String[] args) {
+        _04GroupAnagrams groupAnagrams = new _04GroupAnagrams();
+        String[] strs = {"act","pots","tops","cat","stop","hat"};
+        List<List<String>> result = groupAnagrams.groupAnagrams(strs);
+        result.forEach(System.out::println);
     }
 }
